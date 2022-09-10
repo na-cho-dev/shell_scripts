60119b0e9953e16239472d5ad4a6539b02278329
Nacho369 <nachodev369@gmail.com>
2022-09-10 23:46:07 +0100

    Shell Script Updated

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: Testing
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	printf("Hello, I love Shell Scripting!\n");
+
+	return (0);
+}
